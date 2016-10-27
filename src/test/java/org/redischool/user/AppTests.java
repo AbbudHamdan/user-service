f7409641463dd1a8cd4c6e4d4117c3d66efd61b5
avramesc <ext-aurel.avramescu@here.com>
2016-10-28 00:06:38 +0200

    Dummy assertion

--- src/test/java/org/redischool/user/AppTests.java
+++ src/test/java/org/redischool/user/AppTests.java
@@ -1,5 +1,6 @@
 package org.redischool.user;
 
+import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,7 @@ public class AppTests {
 
 	@Test
 	public void contextLoads() {
+		assertTrue(true);
 	}
 
 }
